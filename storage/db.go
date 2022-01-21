09a33516a643d82ee3d3eed585f7fd26f8fe4b7e
zerjioang <zerjioang@users.noreply.github.com>
2022-01-21 14:46:28 +0100

    updated cache implementation
    Signed-off-by: zerjioang <zerjioang@users.noreply.github.com>

--- storage/db.go
+++ storage/db.go
@@ -95,7 +95,7 @@ func (s *ORMDatabase) ReadAll(ctx context.Context, tx *gorm.DB, obj interface{})
 
 // ReadAllWithFields makes a SELECT query and ONLY retrieves selected column names
 func (s *ORMDatabase) ReadAllWithFields(key string, ctx context.Context, tx *gorm.DB, obj interface{}, columns ...string) error {
-	data, err := s.withCache(key, &obj, 10 * time.Minute, func(dst interface{}) error {
+	data, err := s.withCache(key, obj, 10 * time.Minute, func(dst interface{}) error {
 		if tx != nil {
 			// reuse passed tx Db context
 			tx = tx.Select(columns).Find(dst)
